7646113fb325c52d62f08e2b78a6d908d23cf787
Rishiraj Basu <85842873+Rishi0601@users.noreply.github.com>
2026-02-09 20:08:36 +0530

    Added system information to reports and added some info keys to config file

--- playwright/src/main/java/com/rishi/pw/utils/ExtentReportManager.java
+++ playwright/src/main/java/com/rishi/pw/utils/ExtentReportManager.java
@@ -1,9 +1,11 @@
 package com.rishi.pw.utils;
 
+import com.aventstack.extentreports.AnalysisStrategy;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReportManager extends AppContext {
 
@@ -13,8 +15,19 @@ public class ExtentReportManager extends AppContext {
 		ExtentSparkReporter extentSparkReporter = new ExtentSparkReporter(
 				System.getProperty("user.dir") + "/target/reports/SummaryReport_"
 						+ HelperMethods.getTimestamp("dd-MM-yyyy-HH-mm-ss") + "/TestReports.html");
+		extentSparkReporter.config().setReportName("FlipKart Automation Test Suite");
+		extentSparkReporter.config().setTheme(Theme.DARK);
 		extentReports = new ExtentReports();
 		extentReports.attachReporter(extentSparkReporter);
+
+		extentReports.setSystemInfo("OS", PropertiesReader.getProperty().getProperty("windows"));
+		extentReports.setSystemInfo("JAVA_VER", PropertiesReader.getProperty().getProperty("java-version"));
+		extentReports.setSystemInfo("PLAYWRIGHT_VER", PropertiesReader.getProperty().getProperty("playwright-version"));
+		extentReports.setSystemInfo("APP_ENV", PropertiesReader.getProperty().getProperty("app-env"));
+		extentReports.setSystemInfo("EXECUTION_VIA", PropertiesReader.getProperty().getProperty("execution-mode"));
+		extentReports.setSystemInfo("USERNAME", PropertiesReader.getProperty().getProperty("username"));
+		extentReports.setAnalysisStrategy(AnalysisStrategy.TEST);
+		
 		return extentReports;
 	}
 
